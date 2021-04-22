a78174ac3a6559fbe9e1f0ceec016c50ab9dec57
Edd Mann <the@eddmann.com>
2021-04-22 13:36:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Slack Release Notifier
---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const core = require('@actions/core');
+const request = require('request');
+const { Octokit } = require('@octokit/action');
+
+const fetchReleaseForEvent = async event => {
+  if (event.deployment_status) {
+    const octokit = new Octokit();
+
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+    const { data } = await octokit.request(
+      'GET /repos/:owner/:repo/releases/tags/:tag',
+      {
+        owner,
+        repo,
+        tag: event.deployment.ref,
+      }
+    );
+
+    return data;
+  }
+
+  return event.release;
+};
+
+const toSlackHeadings = message =>
+  message.replace(/^(#{1,6}(.*))$/gm, '*$2*');
+
+const toSlackUserMentions = message => {
+  const lookup = JSON.parse(core.getInput('gitHubSlackUserLookup'));
+  return Object.keys(lookup).reduce(
+    (body, key) =>
+      body.replace(new RegExp(key, 'g'), '<@' + lookup[key] + '>'),
+    message
+  );
+};
+
+async function run() {
+  try {
+    const release = await fetchReleaseForEvent(
+      JSON.parse(
+        fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+      )
+    );
+
+    if (!release) {
+      return;
+    }
+
+    const message = {
+      text: core.getInput('slackTitle'),
+      blocks: [
+        {
+          type: 'header',
+          text: {
+            type: 'plain_text',
+            text: Object.keys(release).reduce(
+              (subject, key) =>
+                subject.replace(`{${key}}`, release[key]),
+              core.getInput('slackHeading')
+            ),
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: toSlackUserMentions(toSlackHeadings(release.body)),
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'View Release',
+              },
+              url: release.html_url,
+            },
+          ],
+        },
+      ],
+    };
+
+    await request.post(core.getInput('slackWebhookEndpointUrl'), {
+      'Content-Type': 'application/json',
+      body: JSON.stringify(message),
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
